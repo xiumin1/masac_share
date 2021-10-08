51fc7b9d91278d8c9b663de40fd1381fa1bff59e
xiumin <42013223+xiumin1@users.noreply.github.com>
2021-10-08 15:55:56 -0700

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,201 @@
+""""
+Project for Udacity Danaodgree in Deep Reinforcement Learning (DRL)
+Code Expanded and Adapted from Code provided by Udacity DRL Team, 2018.
+"""
+
+import copy
+import random
+import numpy as np
+
+import math
+import torch
+
+import torch.nn.functional as F
+import torch.distributed as dist
+from torch.autograd import Variable
+
+def create_log_gaussian(mean, log_std, t):
+    quadratic = -((0.5 * (t - mean) / (log_std.exp())).pow(2))
+    l = mean.shape
+    log_z = log_std
+    z = l[-1] * math.log(2 * math.pi)
+    log_p = quadratic.sum(dim=-1) - log_z.sum(dim=-1) - 0.5 * z
+    return log_p
+
+def logsumexp(inputs, dim=None, keepdim=False):
+    if dim is None:
+        inputs = inputs.view(-1)
+        dim = 0
+    s, _ = torch.max(inputs, dim=dim, keepdim=True)
+    outputs = s + (inputs - s).exp().sum(dim=dim, keepdim=True).log()
+    if not keepdim:
+        outputs = outputs.squeeze(dim)
+    return outputs
+
+def soft_update(target, source, tau):
+    """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+
+        Params
+        ======
+            source: PyTorch model (weights will be copied from)
+            target: PyTorch model (weights will be copied to)
+            tau (float): interpolation parameter 
+        """
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
+
+def hard_update(target, source):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
+
+# add from https://github.com/shariqiqbal2810/maddpg-pytorch/blob/master/utils/misc.py
+# https://github.com/seba-1511/dist_tuto.pth/blob/gh-pages/train_dist.py
+def average_gradients(model):
+    """ Gradient averaging. """
+    size = float(dist.get_world_size())
+    for param in model.parameters():
+        dist.all_reduce(param.grad.data, op=dist.reduce_op.SUM, group=0)
+        param.grad.data /= size
+
+# https://github.com/seba-1511/dist_tuto.pth/blob/gh-pages/train_dist.py
+def init_processes(rank, size, fn, backend='gloo'):
+    """ Initialize the distributed environment. """
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29500'
+    dist.init_process_group(backend, rank=rank, world_size=size)
+    fn(rank, size)
+
+def onehot_from_logits(logits, eps=0.0):
+    """
+    Given batch of logits, return one-hot sample using epsilon greedy strategy
+    (based on given epsilon)
+    """
+    # get best (according to current policy) actions in one-hot form
+    argmax_acs = (logits == logits.max(1, keepdim=True)[0]).float()
+    if eps == 0.0:
+        return argmax_acs
+    # get random actions in one-hot form
+    rand_acs = Variable(torch.eye(logits.shape[1])[[np.random.choice(
+        range(logits.shape[1]), size=logits.shape[0])]], requires_grad=False)
+    # chooses between best and random actions using epsilon greedy
+    return torch.stack([argmax_acs[i] if r > eps else rand_acs[i] for i, r in
+                        enumerate(torch.rand(logits.shape[0]))])
+
+# modified for PyTorch from https://github.com/ericjang/gumbel-softmax/blob/master/Categorical%20VAE.ipynb
+def sample_gumbel(shape, eps=1e-20, tens_type=torch.FloatTensor):
+    """Sample from Gumbel(0, 1)"""
+    U = Variable(tens_type(*shape).uniform_(), requires_grad=False)
+    return -torch.log(-torch.log(U + eps) + eps)
+
+# modified for PyTorch from https://github.com/ericjang/gumbel-softmax/blob/master/Categorical%20VAE.ipynb
+def gumbel_softmax_sample(logits, temperature):
+    """ Draw a sample from the Gumbel-Softmax distribution"""
+    y = logits + sample_gumbel(logits.shape, tens_type=type(logits.data))
+    return F.softmax(y / temperature, dim=1)
+
+# modified for PyTorch from https://github.com/ericjang/gumbel-softmax/blob/master/Categorical%20VAE.ipynb
+def gumbel_softmax(logits, temperature=1.0, hard=False):
+    """Sample from the Gumbel-Softmax distribution and optionally discretize.
+    Args:
+      logits: [batch_size, n_class] unnormalized log-probs
+      temperature: non-negative scalar
+      hard: if True, take argmax, but differentiate w.r.t. soft sample y
+    Returns:
+      [batch_size, n_class] sample from the Gumbel-Softmax distribution.
+      If hard=True, then the returned sample will be one-hot, otherwise it will
+      be a probabilitiy distribution that sums to 1 across classes
+    """
+    y = gumbel_softmax_sample(logits, temperature)
+    if hard:
+        y_hard = onehot_from_logits(y)
+        y = (y_hard - y).detach() + y
+    return y
+
+def firmmax_sample(logits, temperature, dim=1):
+    if temperature == 0:
+        return F.softmax(logits, dim=dim)
+    y = logits + sample_gumbel(logits.shape, tens_type=type(logits.data)) / temperature
+    return F.softmax(y, dim=dim)
+
+def categorical_sample(probs, use_cuda=False):
+    int_acs = torch.multinomial(probs, 1)
+    if use_cuda:
+        tensor_type = torch.cuda.FloatTensor
+    else:
+        tensor_type = torch.FloatTensor
+    acs = Variable(tensor_type(*probs.shape).fill_(0)).scatter_(1, int_acs, 1)
+    return int_acs, acs
+
+def disable_gradients(module):
+    for p in module.parameters():
+        p.requires_grad = False
+
+def enable_gradients(module):
+    for p in module.parameters():
+        p.requires_grad = True
+
+def sep_clip_grad_norm(parameters, max_norm, norm_type=2):
+    """
+    Clips gradient norms calculated on a per-parameter basis, rather than over
+    the whole list of parameters as in torch.nn.utils.clip_grad_norm.
+    Code based on torch.nn.utils.clip_grad_norm
+    """
+    parameters = list(filter(lambda p: p.grad is not None, parameters))
+    max_norm = float(max_norm)
+    norm_type = float(norm_type)
+    for p in parameters:
+        if norm_type == float('inf'):
+            p_norm = p.grad.data.abs().max()
+        else:
+            p_norm = p.grad.data.norm(norm_type)
+        clip_coef = max_norm / (p_norm + 1e-6)
+        if clip_coef < 1:
+            p.grad.data.mul_(clip_coef)
+
+class OUNoise1:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, seed, mu=0.0, theta=0.1, sigma=.5, sigma_min = 0.05, sigma_decay=.99):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.sigma_min = sigma_min
+        self.sigma_decay = sigma_decay
+        self.seed = random.seed(seed)
+        self.size = size
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+        """Resduce  sigma from initial value to min"""
+        self.sigma = max(self.sigma_min, self.sigma*self.sigma_decay)
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.standard_normal(self.size)
+        self.state = x + dx
+        return self.state
+
+# from https://github.com/songrotek/DDPG/blob/master/ou_noise.py
+class OUNoise:
+    def __init__(self, action_dimension, scale=0.1, mu=0, theta=0.15, sigma=0.2):
+        self.action_dimension = action_dimension
+        self.scale = scale
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+        self.state = np.ones(self.action_dimension) * self.mu
+        self.reset()
+
+    def reset(self):
+        self.state = np.ones(self.action_dimension) * self.mu
+
+    def noise(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
+        self.state = x + dx
+        return self.state * self.scale
